da837895d7c528c38e8dfc01767b3fe3c33aed78
sourtin <william.earley@gmail.com>
2016-07-21 18:09:45 +0100

    Refactored out code that will be commonly used in table generators

--- data/gen_general.py
+++ data/gen_general.py
@@ -4,22 +4,6 @@ from misc import cat_alias
 import re
 
 with open(base % "general", "w") as f:
-    ranges = cprngs_by('age', 'category', 'comb_class')
-
-    age = ranges['age']
-    del age[None]
-    t_age = []
-    for a,rs in age.items():
-        maj = int(a[0])
-        min = int(a[2])
-        dat = "(%d,%d)" % (maj,min)
-        for ri,rj in rs:
-            x = xrng(ri,rj,dat)
-            t_age.append((ri,rj,x))
-    t_age.sort()
-    table("UCD_AGE", "[((u8,u8,u8), (u8,u8,u8), (u8,u8))]",
-            map(lambda x:x[2], t_age), file=f)
-
     blocks = []
     t_blk = []
     for el in db_iter():
@@ -36,29 +20,11 @@ with open(base % "general", "w") as f:
     table("UCD_BLOCK", "[((u8,u8,u8), (u8,u8,u8), UnicodeBlock)]",
             map(lambda x:x[2], t_blk), file=f)
 
-    cat_map = dict(cat_alias)
-    cats = [x for _,x in cat_alias]
-    t_cat = []
-    cat = ranges['category']
-    del cat['Cn']
-    for c,rs in cat.items():
-        cn = "UnicodeCategory::%s" % cat_map[c]
-        for ri,rj in rs:
-            x = xrng(ri,rj,cn)
-            t_cat.append((ri,rj,x))
-    t_cat.sort()
-    enum("UnicodeCategory", cats, file=f)
-    table("UCD_CAT", "[((u8,u8,u8), (u8,u8,u8), UnicodeCategory)]",
-            map(lambda x:x[2], t_cat), file=f)
-
-    t_ccc = []
-    ccc = ranges['comb_class']
-    del ccc[0]
-    for c,rs in ccc.items():
-        cn = "%d" % c
-        for ri,rj in rs:
-            x = xrng(ri,rj,cn)
-            t_ccc .append((ri,rj,x))
-    t_ccc.sort()
-    table("UCD_COMBCLS", "[((u8,u8,u8), (u8,u8,u8), u8)]",
-            map(lambda x:x[2], t_ccc), file=f)
+    ranges = cprngs_by('age', 'category', 'comb_class')
+    def age_transform(a):
+        maj = int(a[0])
+        min = int(a[2])
+        return "(%d,%d)" % (maj,min)
+    transformed("(u8,u8)", age_transform, "UCD_AGE", ranges['age'], None, f)
+    enummed("UnicodeCategory", cat_alias, "UCD_CAT", ranges['category'], 'Cn', f)
+    transformed("u8", str, "UCD_COMBCLS", ranges['comb_class'], 0, f)
--- data/generate.py
+++ data/generate.py
@@ -11,7 +11,7 @@ wrapper = textwrap.TextWrapper(
         subsequent_indent='  ')
 
 def enum(name, vals, file=None):
-    print("#[derive(Clone)]", file=file)
+    print("#[derive(Clone,Copy,Eq,PartialEq,Debug)]", file=file)
     print("pub enum %s {" % name, file=file)
     for line in wrapper.wrap(', '.join(vals)):
         print(line, file=file)
@@ -33,3 +33,35 @@ def xrng(ri, rj, dat):
     rjt = "(%d,%d,%d)" % (rj,rj2,rj3)
 
     return "(%s,%s,%s)" % (rit,rjt,dat)
+
+def transformed(type_sig, transfn, table_name, ranges, default, file=None):
+    t_ = []
+    del ranges[default]
+    for c,rs in ranges.items():
+        cn = transfn(c)
+        for ri,rj in rs:
+            x = xrng(ri,rj,cn)
+            t_.append((ri,rj,x))
+    t_.sort()
+    table(table_name, "[((u8,u8,u8), (u8,u8,u8), %s)]" % type_sig,
+            map(lambda x:x[2], t_), file=file)
+
+def enummed(enum_name, aliases, table_name, ranges, default, file=None):
+    enum_map = dict(aliases)
+    symbols = [x for _,x in aliases]
+    enum(enum_name, symbols, file=file)
+    transformed(enum_name, lambda c: "%s::%s" % (enum_name, enum_map[c]),
+                    table_name, ranges, default, file)
+
+def booled(table_name, ranges, default, file=None):
+    # use unit type so works with search and search_range fns unmodified
+    transformed("()", lambda b:{not default:"()"}[b],
+            table_name, ranges, default, file)
+
+def mapped16(table_name, maps, file=None):
+    maps.sort()
+    t_ = []
+    for x,y in maps:
+        assert(x<65536 and y<65536)
+        t_.append("(%d,%d)" % (x,y))
+    table(table_name, "[(u16,u16)]", t_, file=file)
