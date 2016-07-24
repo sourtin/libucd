6c1d8e6cc06d0087bc5c65e483b4ba0c6a02c796
sourtin <william.earley@gmail.com>
2016-07-24 01:27:25 +0100

    Added generated data

---
+++ src/tables/mod.rs
@@ -0,0 +1,23 @@
+pub mod general;
+pub use self::general::*;
+
+pub mod bidi;
+pub use self::bidi::*;
+
+pub mod misc;
+pub use self::misc::*;
+
+pub mod scripts;
+pub use self::scripts::*;
+
+pub mod function;
+pub use self::function::*;
+
+pub mod bool;
+pub use self::bool::*;
+
+pub mod rem;
+pub use self::rem::*;
+
+pub mod decomp;
+pub use self::decomp::*;
