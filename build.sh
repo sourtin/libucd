6a9e2ade090541c1ddf2a454086f556eab008b21
sourtin <william.earley@gmail.com>
2016-07-25 18:55:26 +0100

    Added travis caching

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-mkdir -p tests/data
-cd data
-./gen_tests.py
-cd ..
+if [ ! -d tests/data ]; then
+    mkdir -p tests/data
+    cd data
+    ./gen_tests.py
+    cd ..
+fi
 cargo build --release --verbose
 cargo test --release --verbose
