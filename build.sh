cee24e95bd20a1101fb67c35e1246ff3dc229969
sourtin <william.earley@gmail.com>
2016-07-25 19:47:52 +0100

    Attempt to fix travis caching

--- build.sh
+++ build.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-if [ ! -d tests/data ]; then
+if [ ! -d tests/data ] || [ $(ls tests/data|wc -l) -lt 99 ]; then
+    echo Building test data...
     mkdir -p tests/data
     cd data
     ./gen_tests.py
     cd ..
+else
+    echo Using cached test data...
+    ls tests/data
+    echo
 fi
 cargo build --release --verbose
 cargo test --release --verbose
